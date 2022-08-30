4253ba5922631f10bb5ca2e0af9afc91a3b0e198
Strongside-87 <thomas.arildtoft@icloud.com>
2022-08-30 12:36:47 +0200

    Initial commit

---
+++ web/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  let button = screen.getByRole("Button");
+  expect(button).toHaveTextContent("Login");
+});
+test(`has login button`, () => {
+  render(<App />);
+
+});
